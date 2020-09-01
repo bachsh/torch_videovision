1b1d65767554add36bb98092664bc9e72793e263
Avi Aminov <aviaminov@gmail.com>
2020-09-02 00:42:44 +0300

    added tests for TemporalJitter

--- tests/test_temporal_jitter.py
+++ tests/test_temporal_jitter.py
@@ -1,4 +1,4 @@
-from tests.utils import get_test_video, sample_img_size
+from tests.utils import get_test_video_color_sequence, sample_img_size
 from torchvideotransforms.video_transforms import TemporalJitter, Compose
 from torchvideotransforms.volume_transforms import ClipToTensor
 
@@ -6,7 +6,7 @@ channel_nb = 3
 
 
 def test_temporal_jitter():
-    clip = get_test_video(10)
+    clip = get_test_video_color_sequence(10)
     temp_jitter_transform = TemporalJitter(n_frames=(5,5))
     video_transform = Compose([
         temp_jitter_transform,
--- tests/utils.py
+++ tests/utils.py
@@ -1,11 +1,17 @@
-# Create dummy video clip
 from PIL import Image
+import numpy as np
 
 sample_img_path = '../data/cat/cat1.jpeg'
 sample_img_size = (200, 300)
 
 
-def get_test_video(clip_size):
+def get_test_video_duplicated_image(clip_size):
     img = Image.open(sample_img_path)
     clip = [img] * clip_size
+    return clip
+
+
+def get_test_video_color_sequence(clip_size):
+    colors = np.linspace(0, 255, clip_size).astype(int)
+    clip = [color*np.ones(sample_img_size+(3,)) for color in colors]
     return clip
